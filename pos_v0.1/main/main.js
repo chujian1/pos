8fe6678dbb2d01207a5c32b31311dde9c49ff18d
chujian1 <791086198@qq.com>
2018-03-26 11:30:52 +0800

    second

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  var text = "***<没钱赚商店>收据***";
+  var itemList = [];
+  var signalPrice ;
+  var totalPrice = 0 ;
+  for(let input of inputs) {
+    if (itemList.some(items => {
+        return (items.item.barcode == input.barcode)
+      }))
+    {
+      itemList.forEach(items => {
+        if (items.item.barcode == input.barcode)
+          items.count++;
+
+      })
+    }
+    else {
+      itemList.push({item:input,count:1});
+    }
+  }
+    for(let i of itemList){
+      signalPrice = (i.item.price * i.count).toFixed(2);
+      text += "\n名称：" + i.item.name + "，数量：" + i.count + i.item.unit +"，单价：" + i.item.price.toFixed(2) +"(元)，小计：" + signalPrice +"(元)";
+      totalPrice += parseFloat(signalPrice);
+  }
+
+    text += "\n----------------------\n总计：" + totalPrice.toFixed(2) + "(元)\n**********************";
+
+
+  console.log(text);
 }
--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  var text = "***<没钱赚商店>收据***";
+  var signalPrice ;
+  var totalPrice = 0 ;
+  for(let i in inputs){
+    signalPrice = (inputs[i].price * inputs[i].count).toFixed(2);
+    text += "\n名称：" + inputs[i].name + "，数量：" + inputs[i].count + inputs[i].unit +"，单价：" + inputs[i].price.toFixed(2) +"(元)，小计：" + signalPrice +"(元)";
+    totalPrice += parseFloat(signalPrice);
+  }
+  text += "\n----------------------\n总计：" + totalPrice.toFixed(2) + "(元)\n**********************";
+
+  console.log(text);
 }
